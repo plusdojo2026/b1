bec2ebf42c5344724efff55321be88eb2eb63e9d
plusdojo2026-Tanoue <tanoue-aoi-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-15 11:38:43 +0900

    カスタム共有

--- source/src/main/java/servlet/CustomServlet.java
+++ source/src/main/java/servlet/CustomServlet.java
@@ -1,33 +1,28 @@
 package servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class CustomServlet
- */
+
 @WebServlet("/CustomServlet")
 public class CustomServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public CustomServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		// ログインページにフォワードする
+//		request.setCharacterEncoding("UTF-8");
+//		response.setContentType("text/html; charset=UTF-8");
+//		response.setCharacterEncoding("UTF-8");
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/custom.jsp");
+		dispatcher.forward(request, response);
 	}
 
 	/**
--- source/src/main/webapp/WEB-INF/jsp/custom.jsp
+++ source/src/main/webapp/WEB-INF/jsp/custom.jsp
@@ -1,12 +1,35 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="UTF-8">
-<title>Insert title here</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FanTable │ タイトル</title>
+    <link rel="stylesheet" href="/b1/css/common.css">
+    <link rel="stylesheet" href="/b1/css/custom.css">
 </head>
 <body>
+    <header>
+    <div class="custom">
+        <h1>カスタム</h1>
+    </div>
+    </header>
+
+    <main>
+
+    </main>
+
+    <footer>
+        <nav>
+            <ul>
+                <li class="nowpage"><img src="/b1/images/home_red.png" class="footer_icon"><br>ホーム</li>
+                <li><img src="/b1/images/custom.png" class="footer_icon"><br>カスタム</li>
+                <li><img src="/b1/images/contest.png" class="footer_icon"><br>コンテスト</li>
+                <li><img src="/b1/images/akinator.png" class="footer_icon"><br>診断</li>
+                <li><img src="/b1/images/mydata.png" class="footer_icon"><br>マイデータ</li>
+            </ul>
+        </nav>
+    </footer>
 
 </body>
 </html>
