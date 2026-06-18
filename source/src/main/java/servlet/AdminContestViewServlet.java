9241fd5806ac51f85abb9bb9e69f7f17634e18f2
plusdojo2026-b-yokota <yokota-miho-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-18 11:33:43 +0900

    result

--- source/src/main/java/servlet/AdminContestViewServlet.java
+++ source/src/main/java/servlet/AdminContestViewServlet.java
@@ -1,6 +1,8 @@
 package servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,32 +12,26 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class AdminContestViewServlet
  */
+
 @WebServlet("/AdminContestViewServlet")
 public class AdminContestViewServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public AdminContestViewServlet() {
-        super();
-        // TODO Auto-generated constructor stub
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        // 文字コードは最初に設定
+        request.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html; charset=UTF-8");
+
+        RequestDispatcher dispatcher =
+            request.getRequestDispatcher("/WEB-INF/jsp/result_admin_contest.jsp");
+
+        dispatcher.forward(request, response);
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doGet(request, response);
+    }
 }
