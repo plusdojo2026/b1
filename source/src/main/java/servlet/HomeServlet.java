e0082c189ebb31f704b9c03d660d9e3f0eeda5ae
plusdojo2026-Tanoue <tanoue-aoi-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-19 11:59:53 +0900

    home

--- source/src/main/java/servlet/HomeServlet.java
+++ source/src/main/java/servlet/HomeServlet.java
@@ -24,33 +24,17 @@ public class HomeServlet extends HttpServlet {
         response.setCharacterEncoding("UTF-8");
 
         // 1. セッションからログイン中のユーザー（DTO）を取得
-        // HttpSession session = request.getSession();
-        // dto.LoginUser loginUser = (dto.LoginUser) session.getAttribute("loginUser");
-
-        // 2. 未ログインの場合はログイン画面へ強制遷移
-        // if (loginUser == null) {
-        //    response.sendRedirect(request.getContextPath() + "/login");
-        //    return;
-        //}
-
-        // 3. テストデータとして画像に合わせ「ミスターバーガー」「Gold」を設定
         HttpSession session = request.getSession();
         
-        
-        
-        
-        //ここにメニュー育成機能追加
-     // ★ 育成機能（計算は他で済んでいる前提）
-        
 
-        // ログイン時にセッションへ入れてある前提
+        // 2. 未ログインの場合はログイン画面へ強制遷移        
         LoginUser loginUser = (LoginUser) session.getAttribute("loginUser");
 
         if (loginUser == null) {
             response.sendRedirect(request.getContextPath() + "/login");
             return;
         }
-
+        //ここにメニュー育成機能追加
         int id = loginUser.getId();
 
         UsersDAO dao = new UsersDAO();
